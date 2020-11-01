0046bc66ec1d920802659ee2636eea0871dfe9ef
Anuj Prajapati <anujprajapati@IN1-1029124MBP.local>
2020-11-01 17:30:21 +0530

    Initial Commit

---
+++ src/main/java/com/practice/reactorpractice/ReactorpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.reactorpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/reactorpractice/ReactorpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.reactorpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
